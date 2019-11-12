63ea8116392cebf913966ebed82d786e205d21d5
denis-fominykh <fominykhdenis@gmail.com>
2019-11-12 23:33:05 +0200

    a search of todo is added

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -4,12 +4,23 @@ import './search-panel.scss';
 
 class SearchPanel extends Component {
 
+  state = {
+    term: ''
+  };
+
+  onSearchChange = (e) => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onSearchChange(term);
+  }
 
   render() {
     return (
       <input type="text"
-                className="form-control search-input"
-                placeholder="type to search" />
+             className="form-control search-input"
+             placeholder="type to search"
+             value={ this.state.term }
+             onChange={this.onSearchChange}/>
     );
   }
 }
